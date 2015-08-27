77a127d5bd7bdda200e13286d27768d4b87822a0
ifrond <info@ifrond.com>
2015-08-27 15:21:33 +0200

    admin log

--- database/migrations/2015_08_15_041810_create_views_table.php
+++ database/migrations/2015_08_15_041810_create_views_table.php
@@ -14,7 +14,7 @@ class CreateViewsTable extends Migration
     {
         Schema::create('views', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('ip', 25)->index('ip')->unsigned()->nullable();
+            $table->string('ip', 25)->index('ip')->nullable();
             $table->integer('user_id')->index('user_id')->unsigned()->nullable();
             $table->integer('viewable_id')->index('viewable_id')->unsigned();
             $table->string('viewable_type')->index('viewable_type');
